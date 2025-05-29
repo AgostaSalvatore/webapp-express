0ac6717eb089ddf06f54afbcd364e0358103ac82
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-30 01:31:45 +0200

    feat: add movie controller with index and show endpoints for fetching movies and reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -8,7 +8,25 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-    console.log("dettaglio film")
+    const { id } = req.params
+
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+
+    const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    connection.query(movieSql, [id], (err, movieResult) => {
+        if (err) return res.status(500).json({ error: "Database Query Failed:" + err });
+
+        if (movieResult.length === 0) return res.status(404).json({ error: "Movie not found" });
+
+        const movie = movieResult[0];
+
+        connection.query(reviewSql, [id], (err, reviewResult) => {
+            if (err) return res.status(500).json({ error: "Database Query Failed:" + err });
+            movie.reviews = reviewResult;
+            res.json(movie);
+        })
+    })
 }
 
 module.exports = {
