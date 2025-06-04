b5b56a2ac60ba172529f130b0a33ca1933d992b1
AgostaSalvatore <agosta.boolean@gmail.com>
2025-06-04 13:58:58 +0200

    feat: add multer middleware and movie router for image upload handling

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middlewares/multer");
 
 const movieController = require("../controllers/movieController");
 
 router.get("/", movieController.index);
 router.get("/:id", movieController.show);
+router.post('/store', upload.single('image'), (req, res) => {
+    res.json(req.file)
+})
 
 module.exports = router;
